958fab7753fd88dff7f3f4e9c1fa9f9ad43af84d
Adnan Munawar <adnan.munawar@live.com>
2017-12-23 14:12:20 -0500

    Passing the object index to CB for setting fields of the correct Obj

--- scripts/chai_client.py
+++ scripts/chai_client.py
@@ -1,6 +1,13 @@
 import rospy
 from chai_msg.msg import ObjectState
 
+class Object:
+    def __init__(self, a_name):
+        self.header = []
+        self.name = a_name
+        self.pose = []
+
+
 
 class ChaiClient:
     def __init__(self):
@@ -9,6 +16,7 @@ class ChaiClient:
         self.search_string_end = '/State'
         self.m_nObjects = 0
         self.m_sub_list = []
+        self.m_obj_list = []
         pass
 
     def process_topics(self):
@@ -21,8 +29,8 @@ class ChaiClient:
         print self.active_topics
         pass
 
-    def obj_sub_cb(self, data):
-        print data.name
+    def obj_sub_cb(self, data, a_objInd):
+        print 'Object Data ', data.name, ' Object Index', a_objInd
 
     def find_objects(self):
         for i in range(len(self.active_topics)):
@@ -32,16 +40,20 @@ class ChaiClient:
                 if prefix_ind >= 0:
                     search_ind = self.active_topics[i][j].find(self.search_string_end)
                     if search_ind >=0:
-                        self.m_nObjects +=1
+                        self.m_obj_list.append(Object)
+                        objInd = self.m_nObjects
+                        self.m_sub_list.append(rospy.Subscriber(self.active_topics[i][j],
+                                                                ObjectState, self.obj_sub_cb,
+                                                                callback_args=objInd))
+                        self.m_nObjects += 1
                         print 'Found state', self.active_topics[i][j], ' No of Objects = ', self.m_nObjects
-                        self.m_sub_list.append(rospy.Subscriber(self.active_topics[i][j], ObjectState, self.obj_sub_cb))
 
 
 def main():
     clientObj = ChaiClient()
     clientObj.process_topics()
     clientObj.find_objects()
-
+    rospy.spin()
 
 if __name__=='__main__':
     main()
