16e3e7b3dd33066749c66c8aafb4a899319010cc
Adnan Munawar <adnan.munawar@live.com>
2017-12-23 20:31:07 -0500

    Restructuring and demo of async data acquisition

--- scripts/chai_client.py
+++ scripts/chai_client.py
@@ -2,7 +2,7 @@ import rospy
 from chai_msg.msg import ObjectState
 from threading import Lock
 import threading
-from geometry_msgs.msg import WrenchStamped
+from geometry_msgs.msg import WrenchStamped, Pose
 import time
 
 
@@ -10,7 +10,7 @@ class Object:
     def __init__(self, a_name):
         self.m_time_stamp = []
         self.m_name = a_name
-        self.m_pose = []
+        self.m_pose = Pose()
         self.m_cmd = WrenchStamped()
         self.m_pub = []
         self.m_sub = []
@@ -50,22 +50,31 @@ class ChaiClient:
         else:
             return None
 
+    def get_obj_handle(self, a_name):
+        obj = self.m_objects_dict.get(a_name)
+        return obj
+
     def set_obj_cmd(self, a_name, fx, fy, fz, nx, ny, nz):
         obj = self.m_objects_dict.get(a_name)
         obj.command(fx, fy, fz, nx, ny, nz)
 
-    def process_topics(self):
-        rospy.init_node('chai_client')
-        self.m_ros_topics = rospy.get_published_topics()
-        pass
+    def start(self):
+        self.start_subs()
+        self.start_pubs()
 
     def start_subs(self):
-        t = threading.Thread(target=rospy.spin)
-        t.start()
+        tsub = threading.Thread(target=rospy.spin)
+        tsub.start()
+
+    def start_pubs(self):
+        tpub = threading.Thread(target=self.run_obj_publishers)
+        tpub.start()
 
     def run_obj_publishers(self):
-        for key, obj in self.m_objects_dict.items():
-            obj.run_publisher()
+        while not rospy.is_shutdown():
+            for key, obj in self.m_objects_dict.items():
+                obj.run_publisher()
+            rospy.sleep(0.001)
 
     def print_active_topics(self):
         print self.m_ros_topics
@@ -76,7 +85,9 @@ class ChaiClient:
         for key, value in self.m_objects_dict.items():
             print key
 
-    def find_objects(self):
+    def create_objs_from_rostopics(self):
+        rospy.init_node('chai_client')
+        self.m_ros_topics = rospy.get_published_topics()
         for i in range(len(self.m_ros_topics)):
             for j in range(len(self.m_ros_topics[i])):
                 prefix_ind = self.m_ros_topics[i][j].find(self.m_search_string_prefix)
@@ -99,15 +110,16 @@ class ChaiClient:
 
 def main():
     clientObj = ChaiClient()
-    clientObj.process_topics()
-    clientObj.find_objects()
+    clientObj.create_objs_from_rostopics()
     clientObj.print_summary()
-
-    clientObj.start_subs()
+    clientObj.start()
     while not rospy.is_shutdown():
-        clientObj.set_obj_cmd('Torus', 0,0,0,0,0,0)
-        clientObj.run_obj_publishers()
-        rospy.sleep(0.001)
+        obj = clientObj.get_obj_handle('Torus')
+        obj.command(0,0,0,0,0,0)
+        print obj.m_name
+        print obj.m_pose.position
+        rospy.sleep(0.1)
+
 
 if __name__=='__main__':
     main()
