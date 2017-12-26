d0bc513708e37a3b2ac1d6e736781e955b8adcfe
Adnan Munawar <adnan.munawar@live.com>
2017-12-26 17:26:44 -0500

    Updated to demonstrate the use of WD timer in chai_cleint

--- scripts/tests/env_test.py
+++ scripts/tests/env_test.py
@@ -1,14 +1,12 @@
 from chai_env import ChaiEnv
-
+import time
 
 
 env = ChaiEnv()
 action = env.action_space
 env.make('Torus')
 env.reset()
-import time
-time.sleep(2)
 for i in range(1,500):
     state, r, d, dict = env.step(env.action_space.sample())
-    print state
-env.reset()
+    print r
+input = raw_input('Enter input')
