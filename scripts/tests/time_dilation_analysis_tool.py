99d9e284d3de15c83ac9372e61493599fdd0b09d
Adnan Munawar <adnan.munawar@live.com>
2017-12-28 15:21:16 -0500

    Converting to a class

--- scripts/tests/time_dilation_analysis_tool.py
+++ scripts/tests/time_dilation_analysis_tool.py
@@ -4,63 +4,72 @@ import matplotlib.pyplot as plt
 import rospy.rostime as time
 
 
-sim_time = []
-wall_time = []
-cur_time = []
-itrs = []
-global i
-i=0
-global first_run, time_adjust
-first_run = True
+class TimeDilationAnalysis:
+    def __init__(self):
+        self.sim_time = []
+        self.wall_time = []
+        self.cur_time = []
+        self.itrs = []
+        self.counter = 0
+        self.first_run = True
+        self.msg_itrs = []
+        self.sim_itrs = []
+        self.time_adjust = 0
+        self.plt = plt
 
-x_axis_str = '(Message Num)'
-dt_str = '(Fixed Time)'
-load_str = '(Haptic Dev Load)'
+        self.x_axis_str = '(Message Num)'
+        self.dt_str = '(Fixed Time)'
+        self.load_str = '(Haptic Dev Load)'
+        pass
+
+    def obj_state_cb(self, data):
+        chai_sim_wall_time = data.header.stamp.to_sec()
+        if chai_sim_wall_time > 0.0:
+            if self.counter % 50 == 0:
+                if self.first_run:
+                    self.time_adjust = rospy.Time.now().to_sec() - chai_sim_wall_time
+                    print 'Adjusting Time Offset'
+                    self.first_run = False
+                self.sim_time.append(chai_sim_wall_time)
+                self.wall_time.append(data.mass)
+                self.cur_time.append(time.Time.now().to_sec() - self.time_adjust)
+                self.msg_itrs.append(data.sim_step)
+                self.sim_itrs.append(data.header.seq)
+            self.counter += 1
+
+    def run(self):
+        rospy.init_node('time_dilation_inspector')
+        sub = rospy.Subscriber('/chai/env/Torus/State', ObjectState, self.obj_state_cb, queue_size=10)
+
+        if self.x_axis_str == '(Message Num)':
+            itrs = self.msg_itrs
+            print 'X Axix = Message Num'
+        elif self.x_axis_str == '(Sim Step Num)':
+            itrs = self.sim_itrs
+            print 'X Axix = Simulation Step Num'
+
+        plt.ylim([0, 22])
+        while not rospy.is_shutdown():
+            if len(itrs) > 0:
+                if 0.1 < self.wall_time[-1] <= 20.0:
+                    ct_axes, = plt.plot(itrs, self.cur_time)
+                    wt_axes, = plt.plot(itrs, self.wall_time)
+                    st_axes, = plt.plot(itrs, self.sim_time)
+                    self.plt.grid(True)
+                    self.plt.xlabel(self.x_axis_str)
+                    self.plt.ylabel('(Time)')
+                    self.plt.setp(wt_axes, color='r', linewidth=4.0)
+                    self.plt.setp(st_axes, color='g', linewidth=1.0, marker='|', markersize=10)
+                    self.plt.setp(ct_axes, color='b', linewidth=1.0, marker='x', markersize=10)
+                    self.plt.legend([wt_axes, st_axes, ct_axes], ['Chai Wall Time', 'Simulation Time', 'Current Time'])
+                    self.plt.draw()
+                    title_str = self.load_str + '+' + self.x_axis_str + '+' + self.dt_str
+                    self.plt.title(title_str)
+                    self.plt.pause(0.001)
+
+        plt.savefig(title_str + '.png', bbox_inches='tight')
+        plt.show()
 
 
-def msg_cb(data):
-    global i, first_run, time_adjust
-    chai_sim_wall_time = data.header.stamp.to_sec()
-    if chai_sim_wall_time > 0.0:
-        if i % 50 == 0:
-            if first_run:
-                time_adjust = rospy.Time.now().to_sec() - chai_sim_wall_time
-                print 'Adjusting Time Offset'
-                first_run = False
-
-            sim_time.append(chai_sim_wall_time)
-            wall_time.append(data.mass)
-            cur_time.append(time.Time.now().to_sec() - time_adjust)
-            # itrs.append(data.sim_step)
-            itrs.append(data.header.seq)
-        i += 1
-
-
-rospy.init_node('time_dilation_inspector')
-sub = rospy.Subscriber('/chai/env/Torus/State', ObjectState, msg_cb, queue_size=10)
-
-plt.ylim([0,22])
-# plt.xlim([0,22000])
-title_str = 0
-while not rospy.is_shutdown():
-    if len(itrs) > 0:
-        if wall_time[-1] >= 0.1 and wall_time[-1] <= 20.0:
-            ct_axes, = plt.plot(itrs, cur_time)
-            wt_axes, = plt.plot(itrs, wall_time)
-            st_axes, = plt.plot(itrs, sim_time)
-            plt.grid(True)
-            plt.xlabel(x_axis_str)
-            plt.ylabel('(Time)')
-            plt.setp(wt_axes, color='r', linewidth=4.0)
-            plt.setp(st_axes, color='g', linewidth=1.0, marker='|', markersize=10)
-            plt.setp(ct_axes, color='b', linewidth=1.0, marker='x', markersize=10)
-            plt.legend([wt_axes, st_axes, ct_axes], ['Chai Wall Time', 'Simulation Time', 'Current Time'])
-            plt.draw()
-            title_str = load_str + '+' + x_axis_str + '+' + dt_str
-            plt.title(title_str)
-            plt.pause(0.001)
-
-plt.savefig(title_str + '.png', bbox_inches='tight')
-plt.show()
-
-
+tdObj = TimeDilationAnalysis()
+tdObj.run()
