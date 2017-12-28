9f18b50f8657493e8ec625b2401c4ebeb6d1ef89
Adnan Munawar <adnan.munawar@live.com>
2017-12-28 14:43:39 -0500

    Updated to save figs and a few other minor tweaks

--- scripts/tests/time_dilation_analysis_tool.py
+++ scripts/tests/time_dilation_analysis_tool.py
@@ -13,40 +13,54 @@ i=0
 global first_run, time_adjust
 first_run = True
 
+x_axis_str = '(Message Num)'
+dt_str = '(Fixed Time)'
+load_str = '(Haptic Dev Load)'
+
 
 def msg_cb(data):
     global i, first_run, time_adjust
-    if first_run and data.header.stamp.to_sec() > 0.1:
-        time_adjust = rospy.Time.now().to_sec() - data.header.stamp.to_sec()
-        print 'Adjusting Time Offset'
-        first_run = False
-    if i % 50 == 0 and data.header.stamp.to_sec() > 1.0:
-        sim_time.append(data.header.stamp.to_sec())
-        wall_time.append(data.mass)
-        cur_time.append(time.Time.now().to_sec() - time_adjust)
-        # itrs.append(data.sim_step)
-        itrs.append(data.header.seq)
-    i += 1
+    chai_sim_wall_time = data.header.stamp.to_sec()
+    if chai_sim_wall_time > 0.0:
+        if i % 50 == 0:
+            if first_run:
+                time_adjust = rospy.Time.now().to_sec() - chai_sim_wall_time
+                print 'Adjusting Time Offset'
+                first_run = False
+
+            sim_time.append(chai_sim_wall_time)
+            wall_time.append(data.mass)
+            cur_time.append(time.Time.now().to_sec() - time_adjust)
+            # itrs.append(data.sim_step)
+            itrs.append(data.header.seq)
+        i += 1
 
 
 rospy.init_node('time_dilation_inspector')
 sub = rospy.Subscriber('/chai/env/Torus/State', ObjectState, msg_cb, queue_size=10)
 
-
+plt.ylim([0,22])
+# plt.xlim([0,22000])
+title_str = 0
 while not rospy.is_shutdown():
     if len(itrs) > 0:
         if wall_time[-1] >= 0.1 and wall_time[-1] <= 20.0:
+            ct_axes, = plt.plot(itrs, cur_time)
             wt_axes, = plt.plot(itrs, wall_time)
             st_axes, = plt.plot(itrs, sim_time)
-            ct_axes, = plt.plot(itrs, cur_time)
             plt.grid(True)
-            plt.xlabel('No. Iterations')
-            plt.ylabel('Time')
+            plt.xlabel(x_axis_str)
+            plt.ylabel('(Time)')
             plt.setp(wt_axes, color='r', linewidth=4.0)
-            plt.setp(st_axes, color='g', linewidth=1.0, marker='o', markersize=2)
-            plt.setp(ct_axes, color='b', linewidth=1.0, marker='x', markersize=2)
-            plt.legend([wt_axes, st_axes, ct_axes], ['Chai Wall Time', 'Sim Time', 'Current Time'])
+            plt.setp(st_axes, color='g', linewidth=1.0, marker='|', markersize=10)
+            plt.setp(ct_axes, color='b', linewidth=1.0, marker='x', markersize=10)
+            plt.legend([wt_axes, st_axes, ct_axes], ['Chai Wall Time', 'Simulation Time', 'Current Time'])
             plt.draw()
+            title_str = load_str + '+' + x_axis_str + '+' + dt_str
+            plt.title(title_str)
             plt.pause(0.001)
+
+plt.savefig(title_str + '.png', bbox_inches='tight')
 plt.show()
+
 
