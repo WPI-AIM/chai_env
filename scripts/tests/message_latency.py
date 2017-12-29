f4ec14fdfe93425b8930e99606961aa6383ea5d3
Adnan Munawar <adnan.munawar@live.com>
2017-12-28 20:11:21 -0500

    Using presized np arrays to speed up callback

--- scripts/tests/message_latency.py
+++ scripts/tests/message_latency.py
@@ -2,50 +2,54 @@ import rospy
 from chai_msg.msg import ObjectState
 import matplotlib.pyplot as plt
 import rospy.rostime as time
+import numpy as np
 
 
 class MessageLatency:
     def __init__(self):
-        self.chai_wall_time = []
-        self.cur_wall_time = []
+        self.num_readings = 120000
+        self.chai_wall_time = np.zeros(self.num_readings)
+        self.cur_wall_time = np.zeros(self.num_readings)
         self.counter = 0
         self.first_run = True
-        self.msg_itrs = []
-        self.sim_itrs = []
-        self.latency = []
-        self.time_adjust = 0
+        self.msg_itrs = np.zeros(self.num_readings)
+        self.sim_itrs = np.zeros(self.num_readings)
+        self.latency = np.zeros(self.num_readings)
+        self.time_offset = 0
         self.latency_plt = plt
         self.mean_latency = 0.0
-        self.time_window = [0.1, 20.0]
+        self.time_window = [1.0, 121.0]
         self.done = False
 
         self.x_axis_str = '(Message Num)'
-        self.dt_str = '(Fixed Time)'
-        self.load_str = '(Haptic Dev Load)'
+        self.dt_str = '(Dynamic Time)'
+        self.load_str = '(No Load)'
         pass
 
     def obj_state_cb(self, data):
-        chai_sim_wall_time = data.wall_time
-        process_wall_time = rospy.Time.now().to_sec()
-        if chai_sim_wall_time > 1.0:
-            if self.first_run:
-                self.time_adjust = process_wall_time - chai_sim_wall_time
-                print 'Adjusting Time Offset'
-                self.first_run = False
-            else:
-                cur_wall_time_adjusted = process_wall_time - self.time_adjust
+        if not self.done:
+            chai_sim_wall_time = data.wall_time
+            process_wall_time = rospy.Time.now().to_sec()
+            if chai_sim_wall_time > self.time_window[0]:
+                if self.first_run:
+                    self.time_offset = process_wall_time - chai_sim_wall_time
+                    print 'Adjusting Time Offset: ', self.time_offset
+                    self.first_run = False
+                else:
+                    cur_wall_time_adjusted = process_wall_time - self.time_offset
 
-                self.chai_wall_time.append(chai_sim_wall_time)
-                self.cur_wall_time.append(cur_wall_time_adjusted)
-                self.latency.append(cur_wall_time_adjusted - chai_sim_wall_time)
+                    self.chai_wall_time[self.counter] = chai_sim_wall_time
+                    self.cur_wall_time[self.counter] = cur_wall_time_adjusted
+                    self.latency[self.counter] = cur_wall_time_adjusted - chai_sim_wall_time
 
-                self.sim_itrs.append(data.sim_step)
-                self.msg_itrs.append(data.header.seq)
-            self.counter += 1
+                    self.sim_itrs[self.counter] = data.sim_step
+                    self.msg_itrs[self.counter] = data.header.seq
+                    self.counter += 1
 
     def compute_mean_latency(self):
-        self.mean_latency = sum(self.latency) / len(self.latency)
-        print 'Mean Latency is ', self.mean_latency, ' | No of itrs = ', len(self.latency)
+        self.mean_latency = self.latency[0:self.counter-1].sum() / self.latency[0:self.counter-1].size
+        print 'Mean Latency: ', self.mean_latency, ' | Itrs = ',\
+            self.latency[0:self.counter-1].size, ' | Counter = ', self.counter
 
     def run(self):
         rospy.init_node('message_latency_inspector')
@@ -60,20 +64,20 @@ class MessageLatency:
 
         while not rospy.is_shutdown() and not self.done:
             if len(itrs) > 0:
-                if self.chai_wall_time[-1] > self.time_window[1]:
+                if self.chai_wall_time[self.counter-1] > self.time_window[1]:
                     title_str = self.load_str + '+' + self.x_axis_str + '+' + self.dt_str
                     self.done = True
 
         self.compute_mean_latency()
         self.latency_plt.figure(1)
-        self.latency_plt.hist(self.latency, bins='auto', stacked=True)
+        self.latency_plt.hist(self.latency[0:self.counter-1], bins='auto', stacked=True)
         self.latency_plt.grid(True)
         self.latency_plt.title(title_str)
         self.latency_plt.show()
 
         plt.figure(2)
-        ct, = plt.plot(itrs, self.cur_wall_time, color='r', linewidth=4.0)
-        wt, = plt.plot(itrs, self.chai_wall_time, color='g')
+        ct, = plt.plot(itrs[0:self.counter-1], self.cur_wall_time[0:self.counter-1], color='r', linewidth=4.0)
+        wt, = plt.plot(itrs[0:self.counter-1], self.chai_wall_time[0:self.counter-1], color='g')
         plt.grid(True)
         plt.legend([ct, wt],['Process Wall Time', 'Chai Wall Time'])
         plt.show()
