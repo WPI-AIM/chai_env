5f30c0c95304c782755e4931742cdb7af2ebc1cc
Adnan Munawar <adnan.munawar@live.com>
2017-12-29 09:56:19 -0500

    Using nano secs for time comparison + cleanup and removing redundancies

--- scripts/tests/message_latency.py
+++ scripts/tests/message_latency.py
@@ -2,84 +2,96 @@ import rospy
 from chai_msg.msg import ObjectState
 import matplotlib.pyplot as plt
 import rospy.rostime as time
-import numpy as np
 
 
 class MessageLatency:
     def __init__(self):
-        self.num_readings = 120000
-        self.chai_wall_time = np.zeros(self.num_readings)
-        self.cur_wall_time = np.zeros(self.num_readings)
-        self.counter = 0
-        self.first_run = True
-        self.msg_itrs = np.zeros(self.num_readings)
-        self.sim_itrs = np.zeros(self.num_readings)
-        self.latency = np.zeros(self.num_readings)
-        self.time_offset = 0
-        self.latency_plt = plt
+        self.chai_process_wall_time = []    #Array of times for C++ chai process communicated by RosCom
+        self.cur_process_wall_time = []     #Array of times for current process
+        self.cb_counter = 0
+        self.is_first_run = True
+        self.msg_counter_num = []
+        self.simstep_counter_num = []
+        self.latency = []
+        self.initial_time_offset = 0
         self.mean_latency = 0.0
-        self.time_window = [1.0, 121.0]
+        self.time_window_lims = [1.0, 21.0]
         self.done = False
 
-        self.x_axis_str = '(Message Num)'
-        self.dt_str = '(Dynamic Time)'
-        self.load_str = '(No Load)'
+        self.x_axis_type = 0
+        self.load_type = 0
+        self.dt_type = 0
+
+        self.x_axis_dict = {0: ['(Message Num)', self.msg_counter_num], 1: ['(Sim Step Num)', self.simstep_counter_num]}
+        self.dt_dict = {0: 'Fixed dt = 0.0005', 1: 'Dynamic dt'}
+        self.load_dict = {0: '(No Load)', 1: '(Haptic Dev Load)'}
         pass
 
     def obj_state_cb(self, data):
         if not self.done:
-            chai_sim_wall_time = data.wall_time
-            process_wall_time = rospy.Time.now().to_sec()
-            if chai_sim_wall_time > self.time_window[0]:
-                if self.first_run:
-                    self.time_offset = process_wall_time - chai_sim_wall_time
-                    print 'Adjusting Time Offset: ', self.time_offset
-                    self.first_run = False
+            chai_sim_wall_time = rospy.Time.from_sec(data.wall_time)
+            chai_sim_wall_time = chai_sim_wall_time.to_nsec()
+            process_wall_time = rospy.Time.now().to_nsec()
+            if chai_sim_wall_time > self.time_window_lims[0]:
+                if self.is_first_run:
+                    self.initial_time_offset = process_wall_time - chai_sim_wall_time
+                    print 'Adjusting Time Offset'
+                    print 'Sim Wall Time: ', chai_sim_wall_time
+                    print 'Cur Wall Time: ', process_wall_time
+                    print 'Time Adjust  : ', self.initial_time_offset
+                    self.is_first_run = False
                 else:
-                    cur_wall_time_adjusted = process_wall_time - self.time_offset
+                    cur_wall_time_adjusted = process_wall_time - self.initial_time_offset
 
-                    self.chai_wall_time[self.counter] = chai_sim_wall_time
-                    self.cur_wall_time[self.counter] = cur_wall_time_adjusted
-                    self.latency[self.counter] = cur_wall_time_adjusted - chai_sim_wall_time
+                    self.chai_process_wall_time.append(chai_sim_wall_time)
+                    self.cur_process_wall_time.append(cur_wall_time_adjusted)
+                    self.latency.append(cur_wall_time_adjusted - chai_sim_wall_time)
 
-                    self.sim_itrs[self.counter] = data.sim_step
-                    self.msg_itrs[self.counter] = data.header.seq
-                    self.counter += 1
+                    self.simstep_counter_num.append(data.sim_step)
+                    self.msg_counter_num.append(data.header.seq)
+                self.cb_counter += 1
 
     def compute_mean_latency(self):
-        self.mean_latency = self.latency[0:self.counter-1].sum() / self.latency[0:self.counter-1].size
-        print 'Mean Latency: ', self.mean_latency, ' | Itrs = ',\
-            self.latency[0:self.counter-1].size, ' | Counter = ', self.counter
+        self.mean_latency = sum(self.latency) / len(self.latency)
+        print 'Mean Latency= ', self.mean_latency, ' | Itrs= ', len(self.latency), ' | Counter=', self.cb_counter
 
     def run(self):
         rospy.init_node('message_latency_inspector')
         sub = rospy.Subscriber('/chai/env/Torus/State', ObjectState, self.obj_state_cb, queue_size=1)
 
-        if self.x_axis_str == '(Message Num)':
-            itrs = self.msg_itrs
-            print 'X Axis = Message Num'
-        elif self.x_axis_str == '(Sim Step Num)':
-            itrs = self.sim_itrs
-            print 'X Axis = Simulation Step Num'
+        print 'X Axis = ', self.x_axis_dict[self.x_axis_type][0]
+        itrs = self.x_axis_dict[self.x_axis_type][1]
 
         while not rospy.is_shutdown() and not self.done:
             if len(itrs) > 0:
-                if self.chai_wall_time[self.counter-1] > self.time_window[1]:
-                    title_str = self.load_str + '+' + self.x_axis_str + '+' + self.dt_str
+                temp_time = self.chai_process_wall_time[-1]
+                temp_time = rospy.Time(0, temp_time)
+                temp_time = temp_time.to_sec()
+                if temp_time > self.time_window_lims[1]:
+                    title_str = self.load_dict[self.load_type] +\
+                                '+' + self.x_axis_dict[self.x_axis_type][0] +\
+                                '+' + self.dt_dict[self.dt_type]
                     self.done = True
 
         self.compute_mean_latency()
-        self.latency_plt.figure(1)
-        self.latency_plt.hist(self.latency[0:self.counter-1], bins='auto', stacked=True)
-        self.latency_plt.grid(True)
-        self.latency_plt.title(title_str)
-        self.latency_plt.show()
-
-        plt.figure(2)
-        ct, = plt.plot(itrs[0:self.counter-1], self.cur_wall_time[0:self.counter-1], color='r', linewidth=4.0)
-        wt, = plt.plot(itrs[0:self.counter-1], self.chai_wall_time[0:self.counter-1], color='g')
+        plt.figure(1)
+        plt.subplot(311)
+        plt.hist(self.latency, bins='auto', stacked=True)
         plt.grid(True)
-        plt.legend([ct, wt],['Process Wall Time', 'Chai Wall Time'])
+        plt.title(title_str)
+
+        # plt.figure(2)
+        plt.subplot(312)
+        lt, = plt.plot(itrs, self.latency, color='r', linewidth=1.0)
+        plt.grid(True)
+        plt.legend([lt], ['Latency over time'])
+
+        # plt.figure(3)
+        plt.subplot(313)
+        ct, = plt.plot(itrs, self.cur_process_wall_time, color='r', linewidth=4.0)
+        wt, = plt.plot(itrs, self.chai_process_wall_time, color='g')
+        plt.grid(True)
+        plt.legend([ct, wt], ['Process Wall Time', 'Chai Wall Time'])
         plt.show()
 
 
