4b60f3ff95be9e8d3f65bd02e3f13c89c94bf79e
Adnan Munawar <adnan.munawar@live.com>
2017-12-31 17:49:16 -0500

    Changes to mimic message latency plotting and options

--- scripts/tests/time_dilation_analysis_tool.py
+++ scripts/tests/time_dilation_analysis_tool.py
@@ -9,65 +9,78 @@ class TimeDilationAnalysis:
         self.chai_sim_time = []
         self.chai_wall_time = []
         self.cur_wall_time = []
-        self.itrs = []
+        self.time_window_lims = [0.0, 5.0]
         self.counter = 0
         self.first_run = True
-        self.msg_itrs = []
-        self.sim_itrs = []
-        self.time_adjust = 0
-        self.plt = plt
+        self.done = False
+        self.msg_counter_num = []
+        self.simstep_counter_num = []
+        self.cb_counter_num = []
+        self.initial_time_offset = 0
 
-        self.x_axis_str = '(Message Num)'
-        self.dt_str = '(Fixed Time)'
-        self.load_str = '(Haptic Dev Load)'
+        self.x_axis_type = 0
+        self.load_type = 0
+        self.dt_type = 0
+
+        self.x_axis_dict = {0: ['(Message Num)', self.msg_counter_num],
+                            1: ['(Sim Step Num)', self.simstep_counter_num],
+                            2: ['(Callback Num)', self.cb_counter_num]}
+        self.dt_dict = {0: 'Dynamic dt', 1: 'Fixed dt = 0.0005'}
+        self.load_dict = {0: '(No Load)', 1: '(Haptic Dev Load)'}
         pass
 
+    def capture_window_times(self, time):
+            self.time_window_lims[0] = time + 1.0
+            self.time_window_lims[1] += self.time_window_lims[0]
+            print 'Capturing Time from {} to {}'.format(self.time_window_lims[0], self.time_window_lims[1])
+
     def obj_state_cb(self, data):
-        chai_sim_time = data.chai_sim_time
-        chai_wall_time = data.chai_wall_time
-        if chai_wall_time > 0.0:
-            if self.counter % 50 == 0:
-                if self.first_run:
-                    self.time_adjust = rospy.Time.now().to_sec() - chai_wall_time
-                    print 'Adjusting Time Offset'
-                    self.first_run = False
-                self.chai_sim_time.append(chai_sim_time)
-                self.chai_wall_time.append(chai_wall_time)
-                self.cur_wall_time.append(time.Time.now().to_sec() - self.time_adjust)
-                self.sim_itrs.append(data.sim_step)
-                self.msg_itrs.append(data.header.seq)
-            self.counter += 1
+        if not self.done:
+            chai_sim_time = data.chai_sim_time
+            chai_wall_time = data.chai_wall_time
+            if chai_wall_time > self.time_window_lims[0]:
+                if self.counter % 50 == 0:
+                    if self.first_run:
+                        self.capture_window_times(chai_wall_time)
+                        self.initial_time_offset = rospy.Time.now().to_sec() - chai_wall_time
+                        print 'Adjusting Time Offset'
+                        self.first_run = False
+                    self.chai_sim_time.append(chai_sim_time)
+                    self.chai_wall_time.append(chai_wall_time)
+                    self.cur_wall_time.append(time.Time.now().to_sec() - self.initial_time_offset)
+                    self.simstep_counter_num.append(data.sim_step)
+                    self.msg_counter_num.append(data.header.seq)
+                self.counter += 1
 
     def run(self):
         rospy.init_node('time_dilation_inspector')
         sub = rospy.Subscriber('/chai/env/Torus/State', ObjectState, self.obj_state_cb, queue_size=10)
 
-        if self.x_axis_str == '(Message Num)':
-            itrs = self.msg_itrs
-            print 'X Axix = Message Num'
-        elif self.x_axis_str == '(Sim Step Num)':
-            itrs = self.sim_itrs
-            print 'X Axix = Simulation Step Num'
+        print 'X Axis = ', self.x_axis_dict[self.x_axis_type][0]
+        x_axis_indx = self.x_axis_dict[self.x_axis_type][1]
 
-        plt.ylim([0, 22])
+        title_str = self.load_dict[self.load_type]+\
+                    '+' + self.x_axis_dict[self.x_axis_type][0]+\
+                    '+' + self.dt_dict[self.dt_type]
+        plt.title(title_str)
         while not rospy.is_shutdown():
-            if len(itrs) > 0:
-                if 0.1 < self.chai_wall_time[-1] <= 20.0:
-                    ct_axes, = plt.plot(itrs, self.cur_wall_time)
-                    wt_axes, = plt.plot(itrs, self.chai_wall_time)
-                    st_axes, = plt.plot(itrs, self.chai_sim_time)
-                    self.plt.grid(True)
-                    self.plt.xlabel(self.x_axis_str)
-                    self.plt.ylabel('(Time)')
-                    self.plt.setp(wt_axes, color='r', linewidth=1.0)
-                    self.plt.setp(st_axes, color='g', linewidth=1.0, marker='|', markersize=10)
-                    self.plt.setp(ct_axes, color='b', linewidth=1.0, marker='x', markersize=10)
-                    self.plt.legend([wt_axes, st_axes, ct_axes], ['Chai Wall Time', 'Simulation Time', 'Current Time'])
-                    self.plt.draw()
-                    title_str = self.load_str + '+' + self.x_axis_str + '+' + self.dt_str
-                    self.plt.title(title_str)
-                    self.plt.pause(0.001)
-
+            if len(x_axis_indx) > 0:
+                if self.chai_wall_time[-1] <= self.time_window_lims[1]:
+                    ct_axes, = plt.plot(x_axis_indx, self.cur_wall_time)
+                    wt_axes, = plt.plot(x_axis_indx, self.chai_wall_time)
+                    st_axes, = plt.plot(x_axis_indx, self.chai_sim_time)
+                    plt.grid(True)
+                    plt.xlabel(self.x_axis_dict[self.x_axis_type][0])
+                    plt.ylabel('(Time)')
+                    plt.setp(ct_axes, color='b', linewidth=1.0, marker='o', markersize=6)
+                    plt.setp(wt_axes, color='r', linewidth=1.0, marker='o', markersize=3.5)
+                    plt.setp(st_axes, color='g', linewidth=1.0, marker='o', markersize=2.5)
+                    plt.legend([ct_axes, wt_axes, st_axes], ['Current Time', 'Chai Wall Time', 'Simulation Time'])
+                    plt.draw()
+                    plt.pause(0.001)
+                if self.chai_wall_time[-1] > self.time_window_lims[1]:
+                    self.done = True
+                    break
         plt.savefig(title_str + '.png', bbox_inches='tight')
         plt.show()
 
