1ee8c8615845d1339300aeb8cd0f26984921f633
Adnan Munawar <adnan.munawar@live.com>
2018-01-03 16:05:52 -0500

    Updated to comply with openai's env conventions

--- scripts/chai_env.py
+++ scripts/chai_env.py
@@ -3,17 +3,18 @@ from chai_client import ChaiClient, Object
 from gym import spaces
 import numpy as np
 import time
+import math
 
 
 class Observation:
     def __init__(self):
         self.state = []
-        self.reward = []
+        self.reward = 0.0
         self.is_done = False
-        self.info = {'Empty': 'Empty'}
+        self.info = {}
 
     def cur_observation(self):
-        return self.state, self.reward, self.is_done, self.info
+        return np.array(self.state), self.reward, self.is_done, self.info
 
 
 class ChaiEnv():
@@ -22,12 +23,15 @@ class ChaiEnv():
 
         self.m_chai_client = ChaiClient()
         self.m_chai_client.create_objs_from_rostopics()
-        self.m_chai_client.print_summary()
+        # self.m_chai_client.print_summary()
         self.m_chai_client.start()
 
         self.m_obs = Observation()
-        self.action_space = spaces.Box(-50, 50, shape=(1,6))
-        self.observation_space = spaces.Box(-np.inf, np.inf, shape=(1, 6))
+        self.action_space = spaces.Box(-20, 20, shape=(6,))
+        self.observation_space = spaces.Box(-np.inf, np.inf, shape=(6,))
+
+        self.m_Base = self.m_chai_client.get_obj_handle('Base')
+
         pass
 
     def make(self, a_name):
@@ -36,34 +40,46 @@ class ChaiEnv():
             raise Exception
 
     def reset(self):
-        action = [[0.0,0.0,0.0,0.0,0.0,0.0]]
-        self.step(action)
-        self.update_observation()
-        return self.m_obs.state
+        action = [0.0,0.0,0.0,0.0,0.0,0.0]
+        return self.step(action)[0]
 
     def step(self, action):
-        self.m_obj_handle.command(action[0][0],
-                                  action[0][1],
-                                  action[0][2],
-                                  action[0][3],
-                                  action[0][4],
-                                  action[0][5])
+        self.m_obj_handle.command(action[0],
+                                  action[1],
+                                  action[2],
+                                  action[3],
+                                  action[4],
+                                  action[5])
         time.sleep(0.001)
         self.update_observation()
         return self.m_obs.cur_observation()
 
+    def render(self, mode):
+        print ' I am a {} POTATO'.format(mode)
+
     def update_observation(self):
-        self.m_obs.state = [self.m_obj_handle.get_pose()]
+        self.m_obs.state = self.m_obj_handle.get_pose()
         self.m_obs.reward = self.calculate_reward()
         self.m_obs.is_done = self.check_if_done()
         self.m_obs.info = self.update_info()
 
     def calculate_reward(self):
+        cur_pose = self.m_obj_handle.get_pose()
+        pos_epsilon = 0.5
+        rot_epsilon = 0.1
         reward = 0.0
+        if math.fabs(cur_pose[0]) < pos_epsilon and\
+            math.fabs(cur_pose[1]) < pos_epsilon and\
+            0.926 - math.fabs(cur_pose[2]) < pos_epsilon:
+            reward = 100
+        if math.fabs(cur_pose[3]) < rot_epsilon and\
+            math.fabs(cur_pose[4]) < rot_epsilon and\
+            math.fabs(cur_pose[5]) < rot_epsilon:
+            reward += 50
         return reward
 
     def check_if_done(self):
         return False
 
     def update_info(self):
-        return {'Empty': 'Empty'}
+        return {}
