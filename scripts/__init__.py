1913fcc8431f860c2dd3bea4df22aded5d5275ad
Adnan Munawar <adnan.munawar@live.com>
2018-01-24 16:55:08 -0500

    Updated python setup files

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,1 +1,8 @@
+__all__ = ["chai_client", "chai_env", "chai_object", "chai_world", "watch_dog"]
+
+from chai_client import ChaiClient
+from chai_object import Object
+from chai_world import World
+from chai_env import ChaiEnv
+from watch_dog import WatchDog
 
