5b178173659e7318adca26430af77f9b117837b1
Adnan Munawar <adnan.munawar@live.com>
2018-03-19 16:42:48 -0400

    Appending the states of the base + better reward function

--- scripts/chai_env.py
+++ scripts/chai_env.py
@@ -11,7 +11,8 @@ from numpy import linalg as LA
 
 class Observation:
     def __init__(self):
-        self.state = []
+        self.state = [0]*12
+        self.dist = 0
         self.reward = 0.0
         self.prev_reward = 0.0
         self.cur_reward = 0.0
@@ -38,9 +39,9 @@ class ChaiEnv:
         self.obs = Observation()
         self.action_lims = np.array([30, 30, 30, 2, 2, 2])
         self.action_space = spaces.Box(-self.action_lims, self.action_lims)
-        self.observation_space = spaces.Box(-np.inf, np.inf, shape=(6,))
+        self.observation_space = spaces.Box(-np.inf, np.inf, shape=(12,))
 
-        self.Base = self.chai_client.get_obj_handle('Base')
+        self.base_handle = self.chai_client.get_obj_handle('Base')
         self.prev_sim_step = 0
 
         pass
@@ -82,13 +83,13 @@ class ChaiEnv:
                                      action[4],
                                      action[5])
         self.world_handle.update()
-        self._update_observation()
+        self._update_observation(action)
         return self.obs.cur_observation()
 
     def render(self, mode):
         print ' I am a {} POTATO'.format(mode)
 
-    def _update_observation(self):
+    def _update_observation(self, action):
         if self.enable_step_throttling:
             step_jump = 0
             while step_jump < self.n_skip_steps:
@@ -98,25 +99,19 @@ class ChaiEnv:
             if step_jump > self.n_skip_steps:
                 print 'WARN: Jumped {} steps, Default skip limit {} Steps'.format(step_jump, self.n_skip_steps)
 
-        state = self.obj_handle.get_pose()
+        state = self.obj_handle.get_pose() + self.base_handle.get_pose()
         self.obs.state = state
-        self.obs.reward = self._calculate_reward(state)
+        self.obs.reward = self._calculate_reward(state, action)
         self.obs.is_done = self._check_if_done()
         self.obs.info = self._update_info()
 
-    def _calculate_reward(self, state):
-        max_reward = 1
-        self.obs.prev_reward = self.obs.cur_reward
-        self.obs.cur_reward = max_reward / LA.norm(np.subtract(self.Base.get_pose()[0:3], self.obs.state[0:3]))
-        action_penalty = np.sum(np.square(self.action))
-        if self.obs.cur_reward > self.obs.prev_reward:
-            reward = self.obs.cur_reward
-        elif self.obs.cur_reward < self.obs.prev_reward:
-            reward = 3 * (self.obs.cur_reward - self.obs.prev_reward)
-        else:
-            reward = 0.0
+    def _calculate_reward(self, state, action):
+        prev_dist = self.obs.dist
+        cur_dist = LA.norm(np.subtract(state[6:9], state[0:3]))
+        action_penalty = np.sum(np.square(action))
 
-        reward = reward - action_penalty
+        reward = (prev_dist - cur_dist) - 4 * action_penalty
+        self.obs.dist = cur_dist
         return reward
 
     def _check_if_done(self):
